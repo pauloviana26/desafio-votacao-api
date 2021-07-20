43b08e3ed61b01d8254fba6cbf0d427027064aee
Paulo Viana <pauloc.fviana@hotmail.com>
2021-07-20 11:30:28 -0300

    commit inicial

---
+++ src/test/java/br/com/cooperativaviana/desafiovotacaoapi/DesafioVotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cooperativaviana.desafiovotacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
