db756bfc89cec4352a5b5936c6d0c7358450cdae
Paulo Cesar Faria Viana <paulo.viana@aitec.pt>
2023-02-06 18:32:06 +0000

    SONAR QUBE 4

--- src/test/java/br/com/cooperativaviana/desafiovotacaoapi/DesafioVotacaoApiApplicationTests.java
+++ src/test/java/br/com/cooperativaviana/desafiovotacaoapi/DesafioVotacaoApiApplicationTests.java
@@ -3,11 +3,11 @@ package br.com.cooperativaviana.desafiovotacaoapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DesafioVotacaoApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DesafioVotacaoApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
